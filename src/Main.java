60116b8e0a6d597ed27fbf163367818335934981
thomaslu678 <thomaslu678@gmail.com>
2024-10-28 17:22:14 -0600

    apache commons math working

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import org.apache.commons.math3.stat.StatUtils;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double[] values = {1.0, 2.0, 3.0, 4.0, 5.0};
+        double mean = StatUtils.mean(values);
+        System.out.println("Mean: " + mean);
     }
 }
